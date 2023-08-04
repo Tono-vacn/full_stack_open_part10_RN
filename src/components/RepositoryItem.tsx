f54878a0a0835a790c1ac75951b7f71334e9fe26
Yuchen Jiang <yuchen.jiang@palmpay-inc.com>
2023-08-04 17:35:03 +0800

    10.3

---
+++ src/components/RepositoryItem.tsx
@@ -0,0 +1,108 @@
+import { FlatList, View, StyleSheet,Text, Image } from 'react-native';
+
+const styles = StyleSheet.create({
+  separator: {
+    height: 10,
+  },
+});
+
+const repositories = [
+  {
+    id: 'jaredpalmer.formik',
+    fullName: 'jaredpalmer/formik',
+    description: 'Build forms in React, without the tears',
+    language: 'TypeScript',
+    forksCount: 1589,
+    stargazersCount: 21553,
+    ratingAverage: 88,
+    reviewCount: 4,
+    ownerAvatarUrl: 'https://avatars2.githubusercontent.com/u/4060187?v=4',
+  },
+  {
+    id: 'rails.rails',
+    fullName: 'rails/rails',
+    description: 'Ruby on Rails',
+    language: 'Ruby',
+    forksCount: 18349,
+    stargazersCount: 45377,
+    ratingAverage: 100,
+    reviewCount: 2,
+    ownerAvatarUrl: 'https://avatars1.githubusercontent.com/u/4223?v=4',
+  },
+  {
+    id: 'django.django',
+    fullName: 'django/django',
+    description: 'The Web framework for perfectionists with deadlines.',
+    language: 'Python',
+    forksCount: 21015,
+    stargazersCount: 48496,
+    ratingAverage: 73,
+    reviewCount: 5,
+    ownerAvatarUrl: 'https://avatars2.githubusercontent.com/u/27804?v=4',
+  },
+  {
+    id: 'reduxjs.redux',
+    fullName: 'reduxjs/redux',
+    description: 'Predictable state container for JavaScript apps',
+    language: 'TypeScript',
+    forksCount: 13902,
+    stargazersCount: 52869,
+    ratingAverage: 0,
+    reviewCount: 0,
+    ownerAvatarUrl: 'https://avatars3.githubusercontent.com/u/13142323?v=4',
+  },
+];
+
+const ItemSeparator = () => <View style={styles.separator} />;
+
+const RepositoryItem = ({ item }:any) => {
+  // the item object is passed to this component by the FlatList component
+  // the item object contains the data that the FlatList component iterates over
+  // the item object contains the data for a single repository
+  // the item object is destructured to access the data for a single repository
+  const {
+    fullName,
+    description,
+    language,
+    forksCount,
+    stargazersCount,
+    ratingAverage,
+    reviewCount,
+    ownerAvatarUrl
+  } = item;
+
+  return (
+    <View>
+      <Text>Full name: {fullName}</Text>
+      <Text>Description: {description}</Text>
+      <Text>Language: {language}</Text>
+      <Text>Stars: {stargazersCount}</Text>
+      <Text>Forks: {forksCount}</Text>
+      <Text>Reviews: {reviewCount}</Text>
+      <Text>Rating: {ratingAverage}</Text>
+      <Image
+        style={{ width: 50, height: 50 }}
+        source={{ uri: ownerAvatarUrl }}
+      />
+    </View>
+  );
+}
+
+const RepositoryList = () => {
+  return (
+    <FlatList
+      data={repositories}
+      ItemSeparatorComponent={ItemSeparator}
+      renderItem={
+        // the renderItem prop is a function that returns a component
+        // the function is called for each item in the data array
+        // the function is called with an object that contains the data for a single repository
+        // the object is destructured to access the data for a single repository
+        ({ item }) => <RepositoryItem item={item} />
+      }
+      // other props
+    />
+  );
+};
+
+export default RepositoryList;
